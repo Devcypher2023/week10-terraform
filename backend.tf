a687f2d8d490051df5f4ecac10b752adcb27b222
Devcypher2023 <charleston5000@yahoo.com>
2023-10-29 17:37:44 -0500

    week10

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "week10-cm-bucket"
+    key            = "week10/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "TerraformLock1"  #Lock the bucket
+  }
+}
